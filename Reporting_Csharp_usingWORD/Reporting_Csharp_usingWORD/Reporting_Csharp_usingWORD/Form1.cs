aca9fd54c8401bf82e22129bd28f770c1bfa6da2
Andrew Caleb Baay <baayandrew@gmail.com>
2020-03-02 10:32:15 +0800

    Revert "rename"
    This reverts commit 71094400b20b6f1fffe3c271158e493a3a3735e8.

--- Reporting_Csharp_usingWORD/Reporting_Csharp_usingWORD/Reporting_Csharp_usingWORD/Form1.cs
+++ Reporting_Csharp_usingWORD/Reporting_Csharp_usingWORD/Reporting_Csharp_usingWORD/Form1.cs
@@ -276,6 +276,19 @@ namespace Reporting_Csharp_usingWORD
         }
         #endregion
 
+        private void label1_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tCompany_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
